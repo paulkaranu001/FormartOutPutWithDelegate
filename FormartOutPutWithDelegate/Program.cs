6ca3b24467a5f14d9840523bef2694a6890c3528
paulkaranu001 <paulkaranu@live.com>
2024-07-31 01:08:58 +0300

    Add project files.

---
+++ FormartOutPutWithDelegate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public enum Severity
+{
+    Verbose,
+    Trace,
+    Information,
+    Critital,
+    Warning,
+    Error
+}
+public static class Logger
+{
+    public static Action<string>? WriteMessage;
+    public static Severity LogLevel {  get; set; } = Severity.Warning;
+    public static void LogMessage(Severity s, string component, string msg)
+    {
+        if(s <  LogLevel)
+        {
+            return;
+        }
+        var outputMsg = $"{DateTime.Now}\t{s}\t{component}\t{msg}";
+        if (WriteMessage is not null) 
+        {
+            WriteMessage(outputMsg);
+        }
+
+    }
+    public static void Main()
+    {
+
+
+    }
+
+}
